64e0872025caaa20528a3d811a3af67399119308
ItsDavidHdez <david-0610@live.com.mx>
2025-02-20 16:20:31 -0600

    fix: test db main file

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log(
-    '🔍 MONGO_URI en Railway después de dotenv:',
-    process.env.MONGO_URI,
-  );
-
-  if (!process.env.MONGO_URI) {
-    console.error('❌ ERROR: MONGO_URI no está definido en Railway');
-    process.exit(1);
-  }
-
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
   console.log(`🚀 Servidor corriendo en Railway`);
