f0e4ae90be9b2bcb838b3542e4a63886e17a935a
ItsDavidHdez <david-0610@live.com.mx>
2025-02-20 17:26:30 -0600

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(process.env.PORT || 3000);
-  console.log(`🚀 Servidor corriendo en Railway`);
+  console.log(`🚀 Servidor corriendo en el puerto ${process.env.PORT || 3000}`);
 }
+
 bootstrap();
