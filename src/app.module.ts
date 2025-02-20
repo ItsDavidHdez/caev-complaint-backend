4ee84f128057b9d7eae9efc0ef2eb1b457d31a29
ItsDavidHdez <david-0610@live.com.mx>
2025-02-20 16:18:50 -0600

    fix: test db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,10 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://fallback-uri'),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI ||
+        'mongodb+srv://admin:admin1234@complaints-db.q3bw0.mongodb.net/?retryWrites=true&w=majority&appName=complaints-db',
+    ),
     ComplaintsModule,
     AuthModule,
   ],
