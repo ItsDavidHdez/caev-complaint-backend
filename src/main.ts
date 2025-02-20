2248db1088cedb1fe2412826dfa917dd2e418e54
ItsDavidHdez <david-0610@live.com.mx>
2025-02-20 13:46:06 -0600

    fix: force dotenv upload envs

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,22 @@
-import 'dotenv/config'; // Carga automáticamente el .env
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  console.log('🔍 MONGO_URI en Railway:', process.env.MONGO_URI);
+  console.log(
+    '🔍 MONGO_URI en Railway después de dotenv:',
+    process.env.MONGO_URI,
+  );
 
   if (!process.env.MONGO_URI) {
     console.error('❌ ERROR: MONGO_URI no está definido en Railway');
     process.exit(1);
   }
+
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-      stopAtFirstError: true,
-    }),
-  );
-
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
+  console.log(`🚀 Servidor corriendo en Railway`);
 }
 bootstrap();
