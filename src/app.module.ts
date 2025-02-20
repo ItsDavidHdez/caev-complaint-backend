314d6cecee16cc6286606890c7dae66cd739757d
ItsDavidHdez <david-0610@live.com.mx>
2025-02-20 13:32:05 -0600

    fix: use mongoose module for root database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ComplaintsModule } from './complaints/complaints.module';
 import { AuthModule } from './auth/auth.module';
-import { DatabaseProvider } from './database/database.providers';
 
 @Module({
-  imports: [DatabaseProvider, ComplaintsModule, AuthModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://fallback-uri'),
+    ComplaintsModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
