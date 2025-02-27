247a3d37f1be2287471437cc769bef75d8e58621
ItsDavidHdez <david-0610@live.com.mx>
2025-02-26 21:23:06 -0600

    fix: listen port

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://caev-complaint-frontend-wnzk.vercel.app'],
+    origin: 'https://caev-complaint-frontend-wnzk.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
-
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(
     `🚀 Servidor corriendo en el puerto ${process.env.PORT || 3000}!`,
   );
