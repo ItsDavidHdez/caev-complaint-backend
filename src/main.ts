accfbe7a936776245b5492662381c08a9693d778
ItsDavidHdez <david-0610@live.com.mx>
2025-02-26 21:08:42 -0600

    fix: message log

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,9 @@ async function bootstrap() {
   });
 
   await app.listen(process.env.PORT || 3000);
-  console.log(`🚀 Servidor corriendo en el puerto ${process.env.PORT || 3000}`);
+  console.log(
+    `🚀 Servidor corriendo en el puerto ${process.env.PORT || 3000}!`,
+  );
 }
 
 bootstrap();
